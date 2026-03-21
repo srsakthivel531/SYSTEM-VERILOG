46ccc7d6f4a2d11439875ed3b74a69327f65f399
Sakthivel S <srsakthivel531@gmail.com>
2026-03-21 14:17:41 +0530

    Create 1_packed -1dim.v

---
+++ 2_ARRAY/1_FIXED ARRAY/1_packed -1dim.v
@@ -0,0 +1,18 @@
+//FIXED ARRAY (packed array)
+module array_1D;
+bit [7:0]a;
+initial begin 
+  a='hfx;
+  $display("packed array=%0b",a);
+  #5;
+  a='hfx;
+  $display("packed array=%0d",a);
+  #5;
+  a='hfx;
+  $display("packed array=%0o",a);
+end 
+endmodule 
+//OUTPUT 
+packedarray=11110000
+packedarray=240
+packedarray=360
