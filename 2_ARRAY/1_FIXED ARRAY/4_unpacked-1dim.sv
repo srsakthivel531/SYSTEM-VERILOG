823a81284dc6e322c7851326b8a86e98fbf6c387
Sakthivel S <srsakthivel531@gmail.com>
2026-03-21 18:54:06 +0530

    Update and rename 4_unpacked-1dim.v to 4_unpacked-1dim.sv

--- 2_ARRAY/1_FIXED ARRAY/4_unpacked-1dim.sv
+++ 2_ARRAY/1_FIXED ARRAY/4_unpacked-1dim.sv
@@ -2,7 +2,7 @@
 module array_1D;
   byte a[5]='{1,2,3,4,5};
 initial begin
-  $display("$time=%0t,ENTIRE ARRAY=%0p",$time,a);
+  $display("$time=%0t,ENTIRE ARRAY=%p",$time,a);
   #5;
 foreach(a[i])begin
   a[i]=$urandom_range(5,15);
@@ -11,7 +11,7 @@ end
 end 
 endmodule 
 //OUTPUT
-# KERNEL: $time=0,ENTIRE ARRAY=1 2 3 4 5
+# KERNEL: $time=0,ENTIRE ARRAY='{1, 2, 3, 4, 5}
 # KERNEL: $time=5,a[0]=5
 # KERNEL: $time=5,a[1]=9
 # KERNEL: $time=5,a[2]=10
