f9f4fb4de7cf8845b3d404696cd4c3a0485dbe50
Sakthivel S <srsakthivel531@gmail.com>
2026-03-22 13:30:34 +0530

    Update 2_packed-2dim.sv

--- 2_ARRAY/1_FIXED ARRAY/2_packed-2dim.sv
+++ 2_ARRAY/1_FIXED ARRAY/2_packed-2dim.sv
@@ -1,13 +1,20 @@
 //FIXED ARRAY (packed array)
 module array_1D;
   logic [2][4]a;
+  int i,j;
 initial begin 
-  a='hccb;
-  foreach(a[i])begin 
-    $display("a[%0d]=%b",i,a[i]);
+  a='hcb;
+  foreach(a[i,j])begin 
+    $display("a[%0d][%0d]=%h",i,j,a[i][j]);
   end 
 end 
 endmodule
-//OUTPUT
-# KERNEL: a[1]=1100
-# KERNEL: a[0]=1011
+OUTPUT:
+# KERNEL: a[0][0]=1
+# KERNEL: a[0][1]=1
+# KERNEL: a[0][2]=0
+# KERNEL: a[0][3]=0
+# KERNEL: a[1][0]=1
+# KERNEL: a[1][1]=0
+# KERNEL: a[1][2]=1
+# KERNEL: a[1][3]=1
