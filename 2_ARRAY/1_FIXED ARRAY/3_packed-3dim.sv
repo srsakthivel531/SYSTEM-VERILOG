29e258fb56fb00ecb933c899306c859f3c98da9e
Sakthivel S <srsakthivel531@gmail.com>
2026-03-22 13:32:57 +0530

    Update 3_packed-3dim.sv

--- 2_ARRAY/1_FIXED ARRAY/3_packed-3dim.sv
+++ 2_ARRAY/1_FIXED ARRAY/3_packed-3dim.sv
@@ -3,11 +3,21 @@ module array_3D;
   logic [1:0][1:0][2:0]a;
 initial begin 
   a='hccb;
-  foreach(a[i])begin 
-    $display("a[%0d]=%b",i,a[i]);
+  foreach(a[i,j,k])begin 
+    $display("a[%0d][%0d][%0d]=%b",i,j,k,a[i][j][k]);
   end 
 end 
-endmodule 
-//OUTPUT
-# KERNEL: a[1]=110011
-# KERNEL: a[0]=001011
+endmodule
+OUTPUT:
+ KERNEL: a[1][1][2]=1
+# KERNEL: a[1][1][1]=1
+# KERNEL: a[1][1][0]=0
+# KERNEL: a[1][0][2]=0
+# KERNEL: a[1][0][1]=1
+# KERNEL: a[1][0][0]=1
+# KERNEL: a[0][1][2]=0
+# KERNEL: a[0][1][1]=0
+# KERNEL: a[0][1][0]=1
+# KERNEL: a[0][0][2]=0
+# KERNEL: a[0][0][1]=1
+# KERNEL: a[0][0][0]=1
