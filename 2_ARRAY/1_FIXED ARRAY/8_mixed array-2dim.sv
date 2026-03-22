3d5eed6c3fb179e7f0baa378f0fcf0563e6a6606
Sakthivel S <srsakthivel531@gmail.com>
2026-03-22 13:40:03 +0530

    Update 8_mixed array-2dim.sv

--- 2_ARRAY/1_FIXED ARRAY/8_mixed array-2dim.sv
+++ 2_ARRAY/1_FIXED ARRAY/8_mixed array-2dim.sv
@@ -1,19 +1,24 @@
 //FIXED ARRAY (mixed array)
 module array_2D;
-logic[3:0] a[5][2]='{{1,2},{3,4},{5,7},{8,9},{6,12}};
-int i;
+  logic[3:0] a[5][2];
 initial begin
+ a='{{1,2},{3,4},{5,7},{8,9},{6,12}};
  $display("$time=%0t,ENTIRE ARRAY=%p",$time,a);
   #5;
-foreach(a[i])begin
- $display("$time=%0t,a[%0d]=%p",$time,i,a[i]);
+  foreach(a[i,j])begin
+    $display("$time=%0t,a[%0d][%0d]=%0d",$time,i,j,a[i][j]);
 end 
 end 
 endmodule
-//OUTPUT 
-# KERNEL: $time=0,ENTIRE ARRAY='{'{1, 2}, '{3, 4}, '{5, 7}, '{8, 9}, '{6, 12}}
-# KERNEL: $time=5,a[0]='{1, 2}
-# KERNEL: $time=5,a[1]='{3, 4}
-# KERNEL: $time=5,a[2]='{5, 7}
-# KERNEL: $time=5,a[3]='{8, 9}
-# KERNEL: $time=5,a[4]='{6, 12}
+OUTPUT:
+KERNEL: $time=0,ENTIRE ARRAY='{'{1, 2}, '{3, 4}, '{5, 7}, '{8, 9}, '{6, 12}}
+# KERNEL: $time=5,a[0][0]=1
+# KERNEL: $time=5,a[0][1]=2
+# KERNEL: $time=5,a[1][0]=3
+# KERNEL: $time=5,a[1][1]=4
+# KERNEL: $time=5,a[2][0]=5
+# KERNEL: $time=5,a[2][1]=7
+# KERNEL: $time=5,a[3][0]=8
+# KERNEL: $time=5,a[3][1]=9
+# KERNEL: $time=5,a[4][0]=6
+# KERNEL: $time=5,a[4][1]=12
