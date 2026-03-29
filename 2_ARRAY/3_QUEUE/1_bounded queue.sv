4c21ca7b141211a1ad46d3d37565f6c566e3fb8b
Sakthivel S <srsakthivel531@gmail.com>
2026-03-29 22:25:33 +0530

    Create 1_bounded queue.sv

---
+++ 2_ARRAY/3_QUEUE/1_bounded queue.sv
@@ -0,0 +1,54 @@
+//QUEUE 
+module queue;
+int b1[$:5];//bounded
+initial begin 
+  b1='{10,20,50};
+$display("size of bounded queue=%0d",b1.size());
+//insert
+  b1.insert(1,15);
+  $display("after inserting=%p",b1);
+//push front
+  b1.push_front(5);
+$display("after push_front=%p",b1);
+//push back
+  b1.push_back(100);
+  $display("after push_back=%p",b1);
+//pop front
+  b1.pop_front();
+  $display("after pop_front=%p",b1);
+//pop back 
+  b1.pop_back();
+  $display("after pop_back=%p",b1);
+//reversing
+  b1.reverse();
+  $display("after reversing=%p",b1);
+//sorting
+  b1.sort();
+  $display("after sorting=%p",b1);
+//reverse sorting
+  b1.rsort();
+  $display("after rev_sorting=%p",b1);
+//shuffle
+  b1.shuffle(); 
+  $display("after shuffle=%p",b1);
+//delete the specific index value
+  b1.delete(2); 
+  $display("after delete=%p",b1);
+//deleting full array
+ b1.delete();
+  $display("after delete the whole array=%p",b1);
+end 
+endmodule
+OUTPUT:
+# KERNEL: size of bounded queue=3
+# KERNEL: after inserting='{10, 15, 20, 50}
+# KERNEL: after push_front='{5, 10, 15, 20, 50}
+# KERNEL: after push_back='{5, 10, 15, 20, 50, 100}
+# KERNEL: after pop_front='{10, 15, 20, 50, 100}
+# KERNEL: after pop_back='{10, 15, 20, 50}
+# KERNEL: after reversing='{50, 20, 15, 10}
+# KERNEL: after sorting='{10, 15, 20, 50}
+# KERNEL: after rev_sorting='{50, 20, 15, 10}
+# KERNEL: after shuffle='{10, 20, 50, 15}
+# KERNEL: after delete='{10, 20, 15}
+# KERNEL: after delete the whole array='{}
