0ab4ec10033f33c95d87dc52c00e775853206f83
Sakthivel S <srsakthivel531@gmail.com>
2026-04-01 20:18:29 +0530

    Update 2_unbounded queue.sv

--- 2_ARRAY/3_QUEUE/2_unbounded queue.sv
+++ 2_ARRAY/3_QUEUE/2_unbounded queue.sv
@@ -1,9 +1,9 @@
-//QUEUE 
 module queue;
   int b1[$];//unbounded
+  int value;
 initial begin 
   b1='{10,20,50,60,70,80};
-$display("size of bounded queue=%0d",b1.size());
+  $display("size of unbounded queue=%0d",b1.size());
 //insert
   b1.insert(1,15);
   $display("after inserting=%p",b1);
@@ -14,10 +14,12 @@ $display("after push_front=%p",b1);
   b1.push_back(100);
   $display("after push_back=%p",b1);
 //pop front
-  b1.pop_front();
+  value=b1.pop_front();
+  $display("after pop_front_value=%p",value);
   $display("after pop_front=%p",b1);
 //pop back 
-  b1.pop_back();
+  value=b1.pop_back();
+  $display("after pop_back_value=%p",value);
   $display("after pop_back=%p",b1);
 //reversing
   b1.reverse();
@@ -40,11 +42,13 @@ $display("after push_front=%p",b1);
 end 
 endmodule 
 OUTPUT:
- KERNEL: size of bounded queue=6
+# KERNEL: size of unbounded queue=6
 # KERNEL: after inserting='{10, 15, 20, 50, 60, 70, 80}
 # KERNEL: after push_front='{5, 10, 15, 20, 50, 60, 70, 80}
 # KERNEL: after push_back='{5, 10, 15, 20, 50, 60, 70, 80, 100}
+# KERNEL: after pop_front_value=5
 # KERNEL: after pop_front='{10, 15, 20, 50, 60, 70, 80, 100}
+# KERNEL: after pop_back_value=100
 # KERNEL: after pop_back='{10, 15, 20, 50, 60, 70, 80}
 # KERNEL: after reversing='{80, 70, 60, 50, 20, 15, 10}
 # KERNEL: after sorting='{10, 15, 20, 50, 60, 70, 80}
