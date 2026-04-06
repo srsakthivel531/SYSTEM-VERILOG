017c3bb4198af24667743262cfb86a9cc9a50498
Sakthivel S <srsakthivel531@gmail.com>
2026-04-06 15:58:09 +0530

    Update 1_unpacked union.sv

--- 1_DATA TYPES/7_UNION/1_unpacked union.sv
+++ 1_DATA TYPES/7_UNION/1_unpacked union.sv
@@ -1,30 +1,26 @@
-//UNION DATA TYPE 
-typedef union packed {
-  int i;
-  logic[31:0] data;
-} union_packed;
+//UNION DATA TYPE
 module un_pack;
-  union_packed u_p;
-  initial begin 
+  typedef union {
+  int id;
+  byte data;
+  }unpacked_union;
+ 
+  initial begin
+   unpacked_union u_p;
     $display("BEFORE INITAILIZE");
-    $display("packed value=%0h",u_p.data);
-    $display("packed value=%0h",u_p.i);
+    $display("ID=%0h,DATA=%0h",u_p.id,u_p.data);
     $display("AFTER INITIALIZING IN INT I");
-    u_p.i=32'h11223344;
-    $display("packed value=%0h",u_p.data);
-    $display("packed value=%0h",u_p.i);
+    u_p.id=32'h11223344;
+    $display("ID=%0h,DATA=%0h",u_p.id,u_p.data);
     $display("AFTER INITIALIZING IN LOGIC DATA" );
-    u_p.data='h20304050;
-    $display("packed value=%0h",u_p.i);
-    $display("packed value=%0h",u_p.data);
+    u_p.data='h20;
+    $display("ID=%0h,DATA=%0h",u_p.id,u_p.data);
   end 
 endmodule 
 OUTPUT:
-# KERNEL: BEFORE INITAILIZE
-# KERNEL: packed value=xxxxxxxx
-# KERNEL: packed value=0
-# KERNEL: AFTER INITIALIZING IN INT I
-# KERNEL: packed value=11223344
-# KERNEL: packed value=11223344
-# KERNEL: AFTER INITIALIZING IN LOGIC DATA
-# KERNEL: packed value=20304050
+# BEFORE INITAILIZE
+# ID=0,DATA=0
+# AFTER INITIALIZING IN INT I
+# ID=11223344,DATA=44
+# AFTER INITIALIZING IN LOGIC DATA
+# ID=11223320,DATA=20
