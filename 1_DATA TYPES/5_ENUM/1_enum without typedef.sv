737850c1a13d8a851e17c844e5523244862a2e58
Sakthivel S <srsakthivel531@gmail.com>
2026-04-06 20:11:20 +0530

    Update and rename 1_enum using typedef.sv to 1_enum without typedef.sv

--- 1_DATA TYPES/5_ENUM/1_enum without typedef.sv
+++ 1_DATA TYPES/5_ENUM/1_enum without typedef.sv
@@ -1,9 +1,15 @@
- typedef enum {A,B,C}state;
+//WITHOUT USING TYPEDEF 
 module user_enum;
-  state state_1;
+  enum {A,B,C}state_1;
   initial begin
     $display("first value=%0d",state_1.first());
     $display("second value=%0d",state_1.next());
     $display("third value=%0d",state_1.prev());
   end 
 endmodule
+
+
+OUTPUT:
+# KERNEL: first value=0
+# KERNEL: second value=1
+# KERNEL: third value=2
