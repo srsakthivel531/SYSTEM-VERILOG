d6bd87cb0ea882df2b175dbeb7da98cb88df58d2
Sakthivel S <srsakthivel531@gmail.com>
2026-04-07 20:26:04 +0530

    Update 2_packed struct.sv

--- 1_DATA TYPES/6_STRUCTURE/2_packed struct.sv
+++ 1_DATA TYPES/6_STRUCTURE/2_packed struct.sv
@@ -10,12 +10,12 @@ module st_pack;
     s_p.addr=8'haa;
     s_p.data=16'h1234;
     $display("addr=%0h",s_p.addr);
-    $display("data=%0d",s_p.data);
+    $display("data=%0h",s_p.data);
     temp=s_p;
     $display("packed_value=%0h",temp);  
   end 
 endmodule 
 OUTPUT:
 # KERNEL: addr=aa
-# KERNEL: data=4660
+# KERNEL: data=1234
 # KERNEL: packed_value=aa1234
