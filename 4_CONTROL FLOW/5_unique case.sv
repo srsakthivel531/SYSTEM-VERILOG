19baacc9cc0919c4bdba5fd6f99a020f2d948f50
Sakthivel S <srsakthivel531@gmail.com>
2026-04-08 18:51:44 +0530

    Update 5_unique case.sv

--- 4_CONTROL FLOW/5_unique case.sv
+++ 4_CONTROL FLOW/5_unique case.sv
@@ -1,42 +1,41 @@
-//UNIQUE CASE 
 module unique_case;
   logic [1:0] sel;
 initial begin 
   sel=2'b10;
   $display("FIRST CASE");
   unique case(sel)
-    2'b00:$display("MATCHED");
-    2'b01:$display("MATCHED");
-    2'b10:$display("MATCHED");
-    2'b10:$display("MATCHED");
+    2'b00:$display("MATCHED1");
+    2'b01:$display("MATCHED2");
+    2'b10:$display("MATCHED3");
+    2'b10:$display("MATCHED4");
     default:$display("NOT MATCHED");
   endcase
   $display("MORE THAN ONE MATCH IS FOUND AND THE WARNING MESSAGE SHOWN");
   $display("SECOND CASE");
   unique case(sel)
-    2'b00:$display("MATCHED");
-    2'b01:$display("MATCHED");
-    2'b11:$display("MATCHED");
+    2'b00:$display("MATCHED1");
+    2'b01:$display("MATCHED2");
+    2'b11:$display("MATCHED3");
     default:$display("NOT MATCHED");
   endcase
   $display(" NO MATCH IS FOUND AND DEFAULT STATEMENT EXIST THE WARNING MESSAGE NOT SHOWN");
   $display("THIRD CASE");
   unique case(sel)
-    2'b00:$display("MATCHED");
-    2'b01:$display("MATCHED");
-    2'b11:$display("MATCHED");
+    2'b00:$display("MATCHED1");
+    2'b01:$display("MATCHED2");
+    2'b11:$display("MATCHED3");
   endcase
   $display(" NO MATCH IS FOUND AND DEFAULT STATEMENT NOT EXIST THE WARNING MESSAGE  SHOWN");
 end 
 endmodule 
   OUTPUT:
 FIRST CASE
-MATCHED
+MATCHED3
 Warning-[RT-MTOCMUCS] More than one condition match in statement
-testbench.sv, 7
+testbench.sv, 6
   More than one condition matches are found in 'unique case' statement inside 
   unique_case, at time 0ns.
-  Line number 10 and 11 are overlapping.
+  Line number 9 and 10 are overlapping.
 MORE THAN ONE MATCH IS FOUND AND THE WARNING MESSAGE SHOWN
     
 SECOND CASE
@@ -45,7 +44,7 @@ NO MATCH IS FOUND AND DEFAULT STATEMENT EXIST THE WARNING MESSAGE NOT SHOWN
     
 THIRD CASE
 Warning-[RT-NCMUCS] No condition matches in statement
-testbench.sv, 24
+testbench.sv, 23
   No condition matches in 'unique case' statement. 'default' specification is 
   missing, inside unique_case, at time 0ns.
  NO MATCH IS FOUND AND DEFAULT STATEMENT NOT EXIST THE WARNING MESSAGE  SHOWN
